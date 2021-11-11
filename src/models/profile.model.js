1b64aa57ccb460d347fa776b572a2352b208e457
MD ALI SHAHANSHAH <hello.shahanshah@gmail.com>
2021-11-11 22:58:29 +0530

    Update profile.model.js

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,1 +1,24 @@
-//
+const mongoose = require('mongoose');
+
+//creating schema
+const profileSchema = new mongoose.Schema({
+
+    //schema for profile page
+    // coverPic: { type: String, required: false },
+    headline: { type: String, required: false },
+    about: { type: String, required: false },
+    featured: { type: Array, required: false },
+    activities: { type: Array, required: false },
+    experience: { type: Array, required: false },
+    education: { type: Array, required: false },
+    endorsement: { type: Array, required: false },
+    interests: { type: Array, required: false },
+
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+
+module.exports = mongoose.model("profile", profileSchema);
+
