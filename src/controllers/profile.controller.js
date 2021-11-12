407a2a35f8bdd6c1e3043da9b8969b37ad549c50
MD ALI SHAHANSHAH <82999567+hello-snsa@users.noreply.github.com>
2021-11-12 12:19:59 +0530

    Update profile.controller.js

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,60 +1,12 @@
-const fs = require('fs');
-const express = require("express");
+const express = require('express');
 
-const User = require("../models/user.model");
-const Profile = require("../models/profile.model");
-const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
-//Post Routing
-
-
-//Put Routing
-router.patch("/",
-
-    async (req, res) => {
-
-        try {
-            const profile = await Profile.findByIdAndUpdate(req.params.id, req.body, {
-                new: true,
-            }).select('-password');
-
-            return res.status(201).json({ profile: profile })
-
-        } catch (err) {
-            return res.status(400).json({ status: "failed", message: err.message });
-        }
-    });
-
-//Get Routing
-router.get("/", async (req, res) => {
-
-    try {
-        const profile = await Profile.findById(req.params.id).exec();
-
-        return res.status(200).json({ profile: profile });
-
-    } catch (err) {
-        return res.status(400).json({ status: "failed", message: err.message });
-    }
-});
-
-
-
-//Delete Routing
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const profile = await Profile.findByIdAndRemove(req.params.id).select(
-            '-password'
-        );
-        return res.status(201).json({ profile: profile });
-    } catch (e) {
-        return res.status(400).json({ error: e });
-    }
-});
-
-
-
-module.exports = router;
-
+router.get('/', async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).lean().exec();
+		return res.status(200).json({ profile: profile });
+	} catch (e) {
+		return res.status(400).json
+	}
+})
