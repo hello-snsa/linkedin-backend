61fbfa6506022ccc4435d3b29856ac1df37561e6
Chiranjeev <6258860@gmail.com>
2021-11-12 12:32:26 +0530

    added middlewares

--- src/middlewares/protect.js
+++ src/middlewares/protect.js
@@ -1,59 +1,38 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const User = require('../models/user.model');
 
 const verifyToken = (token) => {
-    return new Promise((resolve, reject) => {
-        jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
-            if (err)
-                return reject(err);
-            return resolve(payload);
-        })
-    })
-}
-// step 1 get the token
+  return new Promise((res, rej) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, payload) => {
+      if (err) return rej(err);
+      res(payload);
+    });
+  });
+};
+
 const protect = async (req, res, next) => {
-    const bearer = req.headers.authorization;
+  // get the token
+  const bearer = req?.headers?.authorization;
+  if (!bearer || !bearer.startsWith('Bearer')) {
+    return res.status(401).json({ message: 'No token present' });
+  }
 
-    if (!bearer || !bearer.startsWith("Bearer "))
-        return res.status(401).json({
-            status: "failed",
-            message: "your email or password is not correct",
-        });
+  // check the token
+  const token = bearer.trim().split(' ')[1];
 
+  // get the payload
+  let payload;
+  try {
+    payload = await verifyToken(token);
+  } catch (e) {
+    return res.status(400).json({ message: 'Check your email and password!' });
+  }
 
-    // step 2 verify the token
-    const token = bearer.split("Bearer ")[1].trim();
-    // console.log("token ", token)
-
-
-    // step 3 Retrieve the user . If user exists then good else bad token
-    let payload;
-    try {
-        payload = await verifyToken(token);
-
-    } catch (err) {
-        return res.status(401).json({
-            status: "failed",
-            message: "Your email or password is not correct",
-        });
-    }
-
-    let user;
-    try {
-        user = User.findById(payload.id).lean().exec();
-    } catch (e) {
-        return res.status(500).json({ status: "failed", message: "something went wrong" });
-    }
-
-    if (!user) {
-        return res.status(401).json({
-            status: "failed",
-            message: "Your email or password is not correct",
-        });
-    }
-
-    req.user = user;
-    next();
+  if (!payload || !payload.user) {
+    return res.status(401).json({ message: 'Check your email and password' });
+  }
+  req.user = payload.user;
+  next();
 };
 
 module.exports = protect;
