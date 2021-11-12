b2ee7f2d3504a1e14211fc2ed1b559606ee33606
MD ALI SHAHANSHAH <82999567+hello-snsa@users.noreply.github.com>
2021-11-12 12:16:23 +0530

    Update profile.model.js

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,24 +1,11 @@
 const mongoose = require('mongoose');
 
-//creating schema
-const profileSchema = new mongoose.Schema({
-
-    //schema for profile page
-    // coverPic: { type: String, required: false },
-    headline: { type: String, required: false },
-    about: { type: String, required: false },
-    featured: { type: Array, required: false },
-    activities: { type: Array, required: false },
-    experience: { type: Array, required: false },
-    education: { type: Array, required: false },
-    endorsement: { type: Array, required: false },
-    interests: { type: Array, required: false },
-
-}, {
-    versionKey: false,
-    timestamps: true
+const profileSchema = mongoose.Schema({
+  cover_img: { type: String },
+  description: { type: String },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
 });
 
+const Profile = mongoose.model('profile', profileSchema);
 
-module.exports = mongoose.model("profile", profileSchema);
-
+module.exports = Profile;
