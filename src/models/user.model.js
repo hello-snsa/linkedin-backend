48b6614b107ff93a9ba0be714e23b2ee4bd6bd4c
MD ALI SHAHANSHAH <82999567+hello-snsa@users.noreply.github.com>
2021-11-12 12:15:38 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,31 +1,66 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
+const Comments = require('./comment.model');
+const Likes = require('./like.model');
+const Notification = require('./notification.model');
+const Posts = require('./post.model');
 
-//creating schema
-const userSchema = new mongoose.Schema({
-
-    //schema for signup page
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true, minlength: 8 },
-    phone: { type: String, required: false, minlength: 10, maxlength: 10 },
-    first_name: { type: String, required: true, minlength: 2 },
-    last_name: { type: String, required: true, minlength: 2 },
-
-    //schema for user details
-    country: { type: String, required: false },
-    city: { type: String, required: false },
-    recentJob: { type: String, required: false },
-    employmentType: { type: String, required: false },
-    recentCompany: { type: String, required: false },
-
-    //schema for user photos
-    dp: { type: String, required: false },
-    backgroundImage: { type: String, required: false },
-
-}, {
+const userSchema = mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    phone: { type: String },
+    password: { type: String },
+    profile_img: { type: String },
+    socketId: { type: String },
+    pendingSent: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    pendingReceived: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    connections: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    recommendations: [{ type: mongoose.Schema.Types.ObjectId, ref: 'user' }],
+    organization: { type: String },
+  },
+  {
     versionKey: false,
-    timestamps: true
+    timestamps: true,
+  }
+);
+
+userSchema.pre('save', function (next) {
+  if (!this.isModified('password')) return next();
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
 });
 
+userSchema.pre('findOneAndRemove', async function (next) {
+  const doc = await this.model.findOne(this.getQuery());
+  try {
+    const posts = await Posts.deleteMany({ user: doc._id });
+    const comments = await Comments.deleteMany({ user: doc._id });
+    const likes = await Likes.deleteMany({ user: doc._id });
+    const notifications = await Notification.deleteMany({ user: doc._id });
+    console.log('deleted user: ', posts, comments, likes, notifications);
+    next();
+  } catch (e) {
+    console.log('error while deleting user activities...');
+    next();
+  }
+});
 
-module.exports = mongoose.model("user", userSchema);
+userSchema.methods.checkPassword = function (password) {
+  const passwordHash = this.password;
+  return new Promise((res, rej) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) return rej(err);
+      res(same);
+    });
+  });
+};
 
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
